da7bf524ec6d143c338431117ca49209e7c560d6
konstr17 <cheezzz@list.ru>
2024-01-12 15:57:56 +0300

    Behind Animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React, { useRef, useState } from "react";
+import React, {useMemo, useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
 
-    const [ posts, setPosts ] = useState([ { id: 1, title: 'Javascript', body: 'Description' }, {
-        id: 2,
-        title: 'Javascript1',
-        body: 'Description'
-    }, { id: 3, title: 'Javascript2', body: 'Description' }, {
-        id: 4,
-        title: 'Javascript3',
-        body: 'Description'
-    }, { id: 5, title: 'Javascript4', body: 'Description' }, ]);
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Post title', body: 'a'},
+        {id: 2, title: 'Post title', body: 'b'},
+        {id: 3, title: 'ccc', body: 'title'},
+        {id: 4, title: 'сс', body: '1'},
+        {id: 5, title: 'Javascript4', body: 'Description'},
+    ]);
+
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+
+    const sortedPosts = useMemo(() => {
+        console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТС');
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        return posts;
+    }, [filter.sort, posts]);
+
+    const sortedAndSearchPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
+    }, [filter.query, sortedPosts]);
 
     const createPost = (newPost) => {
-        setPosts([ ...posts, newPost ])
+        setPosts([...posts, newPost]);
+        setModal(false);
     }
 
     // Принимаем пост из дочернего компонента
@@ -25,19 +44,22 @@ function App() {
     }
 
     return (<div className="App">
-        <PostForm create={ createPost }/>
-        { posts.length !== 0
-            ?
-            <PostList remove={ removePost } posts={ posts } title="Посты про JS"/>
-            :
-            <h1 style={ { textAlign: 'center' } }>
-                Посты не были найдены
-            </h1>
-        }
-
+        <MyButton style={{marginTop:'30px'}} onClick={()=>setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal} >
+            <PostForm create={createPost}/>
+        </MyModal>
+        <hr style={{margin: '15px 0'}}/>
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+        <PostList remove={removePost} posts={sortedAndSearchPosts} title="Посты про JS"/>
     </div>);
 }
 
 export default App;
 
 // 1час 05 ми // сортировка постов
+// 1час 30 м // анимации
