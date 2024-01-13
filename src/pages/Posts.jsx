dd849375652ef81694f1ad100fe368afe88b10b0
krexer008 <y.fedor86@mail.ru>
2024-01-13 14:27:54 +0300

    Unlimit lentas (without pagination) Full Complete

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
 import PostFilter from "../components/PostFilter";
@@ -10,31 +10,38 @@ import Loader from "../components/UI/Loader/Loader";
 import {useFetching} from "../hooks/useFetching";
 import {getPageCount} from "../components/utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import {logDOM} from "@testing-library/react";
+import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
 
-    const [ posts, setPosts ] = useState([]);
-    const [ filter, setFilter ] = useState({ sort: '', query: '' });
-    const [ modal, setModal ] = useState(false);
-    const [ totalPages, setTotalPages ] = useState(0);
-    const [ limit, setLimit ] = useState(10);
-    const [ page, setPage ] = useState(1);
+    const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+    const lastElement = useRef();
 
-
-    const [ fetchPosts, isPostsLoading, postError ] = useFetching(async (limit, page) => {
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page);
-        setPosts(response.data);
+        setPosts([...posts, ...response.data]);
         const totalCount = response.headers['x-total-count'];
         setTotalPages(getPageCount(totalCount, limit));
     });
 
+    useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+        setPage(page + 1);
+    });
+
     useEffect(() => {
         fetchPosts(limit, page);
-    }, []);
+    }, [page, limit]);
 
     const createPost = (newPost) => {
-        setPosts([ ...posts, newPost ]);
+        setPosts([...posts, newPost]);
         setModal(false);
     }
 
@@ -44,32 +51,46 @@ function Posts() {
     }
     const changePage = (page) => {
         setPage(page);
-        fetchPosts(limit, page);
     }
 
     return (<div className="App">
-        <MyButton style={ { marginTop: '30px' } } onClick={ () => setModal(true) }>
+        <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
             Создать пользователя
         </MyButton>
-        <MyModal visible={ modal } setVisible={ setModal }>
-            <PostForm create={ createPost }/>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
         </MyModal>
-        <hr style={ { margin: '15px 0' } }/>
+        <hr style={{margin: '15px 0'}}/>
         <PostFilter
-                filter={ filter }
-                setFilter={ setFilter }
+                filter={filter}
+                setFilter={setFilter}
         />
-        { postError &&
-                <h1>Произошла ошибка ${ postError }</h1>
+        <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultValue="Колличество элементов на странице"
+                options={[
+                    {value: 5, name: '5'},
+                    {value: 10, name: '10'},
+                    {value: 25, name: '25'},
+                    {value: -1, name: 'Показать все'},
+                ]}
+        />
+        {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
         }
-        { isPostsLoading
-                ? <div style={ { display: 'flex', justifyContent: 'center', marginTop: '50px' } }><Loader/></div>
-                : <PostList remove={ removePost } posts={ sortedAndSearchPosts } title="Посты про JS"/>
+        <PostList remove={removePost} posts={sortedAndSearchPosts} title="Посты про JS"/>
+        <div ref={lastElement} style={{height: 20, background: 'red'}}></div>
+
+        {isPostsLoading &&
+                <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
         }
+
+
         <Pagination
-                page={ page }
-                changePage={ changePage }
-                totalPages={ totalPages }
+                page={page}
+                changePage={changePage}
+                totalPages={totalPages}
         />
     </div>);
 }
